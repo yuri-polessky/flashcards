4b7002698be8518db1d35cd3f700796b6b4af47b
Iurii Polesskii <neoander000@gmail.com>
2015-09-01 12:15:48 +0600

    implement create,update and destroy cards

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 gem 'pg'
+gem 'simple_form'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,18 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
+  validate  :uniqueness_original_and_translated_text
+  before_validation :set_review_date, on: :create
+
+  private
+    def uniqueness_original_and_translated_text
+      if translated_text.downcase.strip == original_text.downcase.strip
+        errors.add(:translated_text,"can't be same as original text")
+      end
+    end
+
+    def set_review_date
+      self.review_date = Date.current + 3.day
+    end
 end
