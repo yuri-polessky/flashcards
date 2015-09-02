bd0301eb2ec58d188fa50fd7d76220c8d5d5d1a7
Iurii Polesskii <neoander000@gmail.com>
2015-09-03 01:16:27 +0600

    Add method new_review_date to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,12 +4,15 @@ class Review
 
   def check_translation
     if answer.strip.mb_chars.downcase == original_text.strip.mb_chars.downcase
-      card.set_review_date
-      card.save
+      card.update(review_date: new_review_date)
       true
     else
       false
     end
+  end
+
+  def new_review_date
+    Date.current + 3.day
   end
 
   def card
