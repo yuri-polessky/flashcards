4e7c0c39b1d53c71f7befa18b112ce5e7495ab5c
Iurii Polesskii <neoander000@gmail.com>
2015-09-03 13:09:25 +0600

    Create tests for model Card and Review

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ActiveRecord::Base
   validates_presence_of :original_text, :translated_text, :review_date, presence: true
-  validate  :uniqueness_original_and_translated_text
+  validate  :uniqueness_original_and_translated_text, if: [:original_text,:translated_text]
   before_validation :set_review_date, on: :create
 
   scope :for_review, -> { where("review_date <= ?", Date.current) }
---
+++ spec/models/card_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe Card do
+  it "is valid with a original_text, translated_text" do
+    card = Card.new(original_text: "way", translated_text: "путь")
+    expect(card).to be_valid
+  end
+  
+  it "is invalid without a original_text" do
+    card = Card.new(translated_text: "путь")
+    card.valid?
+    expect(card.errors[:original_text]).to include("can't be blank")
+  end
+  
+  it "is invalid without a translated_text" do
+    card = Card.new(original_text: "путь")
+    card.valid?
+    expect(card.errors[:translated_text]).to include("can't be blank")
+  end
+
+  it "has correct review_date after creation" do
+    card = Card.create(original_text: "way", translated_text: "путь")
+    expect(card.review_date).to eq Date.current + 3.day
+  end
+
+  describe "validate original_text and translated_text" do
+    
+    context "with same word" do
+      it "is invalid" do
+        card = Card.new(original_text: "way", translated_text: "way")
+        card.valid?
+        expect(card.errors[:translated_text]).to include("can't be same as original text")
+      end
+    end
+
+    context "with same word in different letter cases " do
+      it "is invalid" do
+        card = Card.new(original_text: "way", translated_text: "WAY")
+        card.valid?
+        expect(card.errors[:translated_text]).to include("can't be same as original text")
+      end
+    end
+
+    context "with same word with leading and trailing space" do
+      it "is invalid" do
+        card = Card.new(original_text: " way", translated_text: "way ")
+        card.valid?
+        expect(card.errors[:translated_text]).to include("can't be same as original text")
+      end
+    end
+  
+  end
+end
