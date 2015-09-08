4794bac3b3bd18d9f0cddff4a2dbca509fcb6808
Iurii Polesskii <neoander000@gmail.com>
2015-09-08 19:25:20 +0600

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,9 +6,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    bucket: ENV["S3_BUCKET"],
-    access_key_id: ENV["S3_KEY"],
-    secret_access_key: ENV["S3_SECRET"]
+    bucket: ENV["S3_BUCKET"]
   }
 
   # Eager load code on boot. This eager loads most of Rails and
