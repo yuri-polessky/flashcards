e65afdfe0a5b8398449c547c2ae00248e1b60ae7
Iurii Polesskii <neoander000@gmail.com>
2015-09-08 19:17:50 +0600

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    bucket: ENV["S3_BUCKET"]
-    access_key_id: ENV["S3_KEY"]
+    bucket: ENV["S3_BUCKET"],
+    access_key_id: ENV["S3_KEY"],
     secret_access_key: ENV["S3_SECRET"]
   }
 
