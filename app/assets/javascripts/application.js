cea6d399ee491141a3d31f1cbeeb38da97901435
Iurii Polesskii <neoander000@gmail.com>
2015-09-16 18:38:10 +0600

    Fix hound violations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,10 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that"ll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// or any plugin"s vendor/assets/javascripts directory can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// It"s not advisable to add code directly here, but if you do, it"ll appear at the bottom of the
 // compiled file.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
@@ -30,24 +30,26 @@ function startTimer() {
 
 function checkTranslation() {
   startTimer();
-  $('#review_form').submit(function(e) {
+  $("#review_form").submit(function(e) {
     e.preventDefault();
-    var sendRequest = $.post('/reviews', $('#review_form').serialize(), null, "json");
+    var sendRequest = $.post("/reviews", $("#review_form").serialize(), null, "json");
     return sendRequest.done(function(data) {
       if (data.state == "wrong") {
-        $("#notice_panel").empty().append($('<div />').attr('class', 'alert alert-info').append(data.message));
+        $("#notice_panel").empty().append($("<div />").
+          attr("class", "alert alert-info").append(data.message));
       } else {
-        $("#notice_panel").empty().append($('<div />').attr('class', 'alert alert-info').append(data.message));
+        $("#notice_panel").empty().append($("<div />").
+          attr("class", "alert alert-info").append(data.message));
         $.getScript("/reviews/new.js");
       }
     });
   });
-};
+}
 
 $(function(){
   checkTranslation();
 });
 $(document).ajaxComplete(function (){
-  $('#review_form').off("submit");
+  $("#review_form").off("submit");
   checkTranslation();
 });
