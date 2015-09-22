ce395f3bfede5b6bc7b00e4514cfc1de5a5cb96e
Iurii Polesskii <neoander000@gmail.com>
2015-09-22 16:22:10 +0600

    Reconfig NewRelic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
     authentication:       :plain,
     user_name:            ENV["SENDGRID_USERNAME"],
     password:             ENV["SENDGRID_PASSWORD"],
-    domain:               'heroku.com',
+    domain:               'ec2-52-88-40-177.us-west-2.compute.amazonaws.com',
     enable_starttls_auto: true 
   }
 
